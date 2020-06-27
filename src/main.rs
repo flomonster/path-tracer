0ebf2c2be8699f504975004fea00bd7105465a41
Alexandre Lamure <alexandre.lamure@epita.fr>
2020-06-27 13:43:38 -0400

    Add simple cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Same as previous examples...
 }
