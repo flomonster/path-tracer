eae908c898245e1fed2d40e0b8447e06071bef7a
Florian Amsallem <florian.amsallem@epita.fr>
2020-11-18 02:03:32 +0100

    clean code

--- src/utils/hit.rs
+++ src/utils/hit.rs
@@ -9,19 +9,26 @@ pub struct Hit {
     pub position: Vector3<f32>,
 
     /// Barycenter
-    pub uv: Vector2<f32>,
+    pub text_coords: Vector2<f32>,
+    /// Normal vector of the triangle at the hit point
+    pub normal: Vector3<f32>,
 }
 
 impl Hit {
-    pub fn normal(&self) -> Vector3<f32> {
-        (1. - self.uv.x - self.uv.y) * self.triangle[0].normal
-            + self.uv.x * self.triangle[1].normal
-            + self.uv.y * self.triangle[2].normal
-    }
+    pub fn new(triangle: Triangle, dist: f32, position: Vector3<f32>, uv: &Vector2<f32>) -> Self {
+        let normal = (1. - uv.x - uv.y) * triangle[0].normal
+            + uv.x * triangle[1].normal
+            + uv.y * triangle[2].normal;
+        let text_coords = triangle[0].texture
+            + uv.x * (triangle[1].texture - triangle[0].texture)
+            + uv.y * (triangle[2].texture - triangle[0].texture);
 
-    pub fn text_coords(&self) -> Vector2<f32> {
-        self.triangle[0].texture
-            + self.uv.x * (self.triangle[1].texture - self.triangle[0].texture)
-            + self.uv.y * (self.triangle[2].texture - self.triangle[0].texture)
+        Self {
+            triangle,
+            dist,
+            position,
+            text_coords,
+            normal,
+        }
     }
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -8,6 +8,6 @@ pub use ray::Ray;
 use cgmath::*;
 
 /// Compute reflection vector given incident and normal vectors
-pub fn reflection(i: &Vector3<f32>, n: &Vector3<f32>) -> Vector3<f32> {
+pub fn _reflection(i: &Vector3<f32>, n: &Vector3<f32>) -> Vector3<f32> {
     i - 2. * i.dot(n.clone()) * n
 }
